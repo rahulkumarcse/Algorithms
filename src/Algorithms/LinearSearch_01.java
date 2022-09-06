d0d7b843e18467ccd29f5d6c835435359882d59c
Rahul Kumar <rahulcsofficial@gmail.comm>
2022-09-06 20:08:10 +0530

    Linear Search completed

--- src/Algorithms/LinearSearch_01.java
+++ src/Algorithms/LinearSearch_01.java
@@ -1,4 +1,18 @@
 package Algorithms;
 
 public class LinearSearch_01 {
+    static int linearSearch(int[] arr,int n){
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==n){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]= {1,2,6,8,5,7};
+        int search=linearSearch(arr,5);
+        System.out.println(search);
+    }
 }
